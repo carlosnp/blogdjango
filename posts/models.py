b85b1121665d1d972dc4af8459148fb7dd028643
carlosnp <machinery.e@gmail.com>
2019-02-19 15:07:25 -0400

    imagefields

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.urls import reverse
 
+# Actualizar la imagen y guardarla en una carpeta con el id del usuario
+def upload_location(instance, filename):
+    # filebase, extension = filename.split(".")
+    return "%s/%s"%(instance.id, filename)
+    # return "%s/%s.%s"%(instance.id, instance.id, extension)
+
 # Create your models here.
 class Post(models.Model):
     title       = models.CharField("Titulo",max_length=120)
-    image       = models.FileField("Imagen", null= True, blank=True)
+    image       = models.ImageField("Imagen", 
+                null=True,
+                blank=True, 
+                upload_to = upload_location,
+                height_field="height_field", 
+                width_field="width_field", 
+                max_length=None)
+    height_field=models.IntegerField(default=0)
+    width_field =models.IntegerField(default=0)
     content     = models.TextField("Contenido")
     updated     = models.DateTimeField("Fecha de Actualizacion",auto_now=True, auto_now_add=False)
     timestamp   = models.DateTimeField("Fecha de creacion",auto_now=False, auto_now_add=True)
