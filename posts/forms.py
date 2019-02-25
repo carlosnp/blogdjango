f3ef714895b8b893cbdf084bd38c91ef0aa53d4c
carlosnp <machinery.e@gmail.com>
2019-02-25 19:27:57 -0400

    Django Pagedown aplicado en list view

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,15 @@
+# Django
 from django import forms
-
+# External APP
+from pagedown.widgets import PagedownWidget
+# Local APP
 from .models import Post
 
 class PostForm(forms.ModelForm):
+	# Formulario de Contenido
+	content = forms.CharField(widget=PagedownWidget)
+	# Formulario de Fecha con un select
+	publish = forms.DateField(label="Publicar el",widget=forms.SelectDateWidget)
 	class Meta:
 		model = Post
 		fields = ["title", "content", "image", "draf", "publish","author"]
