58ecdb21d9735bb2edd0f08d3a20172890751d43
carlosnp <machinery.e@gmail.com>
2019-02-28 11:42:00 -0400

    errors page 404 y 500

--- blogdjango/settings.py
+++ blogdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=p4ikix$n=k2*l@4^e^=_r$&fsz%q7qc&!j^rtrw8wi9)&=8q='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,13 +130,14 @@ THOUSAND_SEPARATOR = '.'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
 ]
 
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+if not DEBUG:
+    print("DEBUG",DEBUG)
