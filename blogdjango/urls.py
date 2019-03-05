43c6389d80deff82de3ff1c04c83e48cce7afe07
carlosnp <machinery.e@gmail.com>
2019-03-05 19:50:27 -0400

    cambios en archivo base

--- blogdjango/urls.py
+++ blogdjango/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls', namespace="posts")),
+    path('', include('posts.urls', namespace="posts")),
 ]
 
 if settings.DEBUG:
