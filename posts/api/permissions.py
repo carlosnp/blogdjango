909088cc736ebd18583af872a296f84bc7b18062
carlosnp <machinery.e@gmail.com>
2019-04-07 08:57:25 -0400

    permisos de usuario

--- posts/api/permissions.py
+++ posts/api/permissions.py
@@ -1,10 +1,18 @@
 # Django
-from rest_framework.permissions import (BasePermission)
+from rest_framework.permissions import (BasePermission, SAFE_METHODS)
 # Project
 
 # Si es el Dueño
 class IsOwnerOrReadOnly(BasePermission):
 	message = 'No Tienes los permisos necesarios para modificar o eliminar un Post de otro usuario'
+	my_safe_method = ['GET','PUT', 'DELETE']
+
+	def has_permission(self, request, view):
+		if request.method in self.my_safe_method:
+			return True
+		return False
 
 	def has_object_permission(self, request, view, obj):
+		if request.method in SAFE_METHODS:
+			return True
 		return obj.author == request.user
