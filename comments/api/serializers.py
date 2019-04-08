e945c2c3cb7dc5f997178746c15128ac5bd2402d
machinery <machinery.e@gmail.com>
2019-04-08 13:45:21 -0400

    se agregan las replicas a os comentarios

--- comments/api/serializers.py
+++ comments/api/serializers.py
@@ -11,17 +11,39 @@ COMMENT_Detail_url = HyperlinkedIdentityField(view_name = 'comments_api:detail',
 class CommentListSerializers(ModelSerializer):
     Detail_url = COMMENT_Detail_url
     author = SerializerMethodField()
+    reply_count = SerializerMethodField()
+    
     class Meta:
         model = Comment
         fields = [
             "Detail_url",
             "id",
-            "object_id",
-            "content_type",
+            #"object_id",
+            #"content_type",
             #"content_object",
-            "parent",
+            #"parent",
             "content", 
-            #"timestamp", 
+            #"timestamp",
+            "reply_count", 
+            "author"]
+    
+    # Cuenta las replicas a un comentario
+    def get_reply_count(self, obj):
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
+    
+    def get_author(self, obj):
+        return str(obj.author.username)
+
+class CommentChildSerializers(ModelSerializer):
+    author = SerializerMethodField()
+    class Meta:
+        model = Comment
+        fields = [
+            "id",
+            "content", 
+            "timestamp", 
             "author"]
     
     def get_author(self, obj):
@@ -29,6 +51,8 @@ class CommentListSerializers(ModelSerializer):
 
 class CommentDetailSerializers(ModelSerializer):
     author = SerializerMethodField()
+    replies = SerializerMethodField()
+    reply_count = SerializerMethodField()
     class Meta:
         model = Comment
         fields = [
@@ -36,10 +60,23 @@ class CommentDetailSerializers(ModelSerializer):
             "object_id",
             "content_type",
             #"content_object",
-            "parent",
             "content", 
-            "timestamp", 
-            "author"]
+            "timestamp",
+            "author",
+            "reply_count", 
+            "replies"]
     
     def get_author(self, obj):
         return str(obj.author.username)
+    
+    # Determina si hay replicas a un comentario
+    def get_replies(self, obj):
+        if obj.is_parent:
+            return CommentChildSerializers(obj.children(), many=True).data
+        return None
+    
+    # Cuenta las replicas a un comentario
+    def get_reply_count(self, obj):
+        if obj.is_parent:
+            return obj.children().count()
+        return 0
