2d7170bb8941009bc22a69bba3a6d5e90084ef93
machinery <machinery.e@gmail.com>
2019-04-08 10:32:24 -0400

    se agregan links para acceder a las paginas de detalles, eliminar, y editar desde django rest

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,17 +1,24 @@
 # Django
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
 # Project
 from posts.models import Post
 
+POST_Detail_url = HyperlinkedIdentityField(view_name = 'posts_api:detail',lookup_field = 'pk')
+POST_Delete_url = HyperlinkedIdentityField(view_name = 'posts_api:delete',lookup_field = 'pk')
+POST_Edit_url   = HyperlinkedIdentityField(view_name = 'posts_api:update',lookup_field = 'pk')
+
 class PostListSerializers(ModelSerializer):
-	class Meta:
-		model = Post
-		fields = ["id","title", "content", "image", "draf", "publish","author"]
+    Detail_url = POST_Detail_url
+    class Meta:
+        model = Post
+        fields = ["Detail_url","title", "content", "image", "draf", "publish","author"]
 
 class PostDetailSerializers(ModelSerializer):
-	class Meta:
-		model = Post
-		fields = ["id","title", "slug","content", "image", "draf", "timestamp", "publish", "updated", "read_time","author"]
+    Delete_url = POST_Delete_url
+    Edit_url = POST_Edit_url
+    class Meta:
+        model = Post
+        fields = ["Edit_url","Delete_url","id","title", "slug","content", "image", "draf", "timestamp", "publish", "updated", "read_time","author"]
 
 class PostCreateUpdateSerializers(ModelSerializer):
 	class Meta:
