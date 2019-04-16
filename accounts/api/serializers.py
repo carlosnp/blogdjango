51a9e3e41815b9f92701183631aa1fbe4011f1d5
machinery <machinery.e@gmail.com>
2019-04-16 14:26:53 -0400

    validando el email con django rest

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -2,9 +2,14 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 # Django Rest Framework
-from rest_framework.serializers import (ModelSerializer, 
-                                        HyperlinkedIdentityField, 
+from rest_framework.serializers import (# Serializer
+										ModelSerializer,
+										# Serializer Fields
+										EmailField,
+										# Serializer Fields Miscellaneous
                                         SerializerMethodField,
+                                        # Serializer Relations
+                                        HyperlinkedIdentityField,
                                         ValidationError)
 
 # Usuario
@@ -12,19 +17,38 @@ User = get_user_model()
 
 # Create User
 class UserCreateSerializer(ModelSerializer):
+	email = EmailField(label='Dirección de Correo Electrónico')
+	email2 = EmailField(label='Confirme Dirección de Correo Electrónico')
 	class Meta:
 		model = User
 		fields = [
 			'username',
 			'email',
+			'email2',
 			'password',
 		]
 		extra_kwargs = {"password": {"write_only": True}
 		}
 
+	# Validando SI existe el correo electronico
+	def validate(self, data):
+		email = data['email']
+		user_qs = User.objects.filter(email=email)
+		if user_qs.exists():
+			raise ValidationError("Esta dirección de Correo Electrónico YA fue registrada")
+		return data
+
+	# Validacion del correo electronico
+	def validate_email2(self, value):
+		data 	= self.get_initial()
+		email1 	= data.get("email")
+		email2 	= value
+		if email1 != email2:
+			raise ValidationError("La Dirección de Correo Electrónico NO coincide.")
+		return value
+
 	# Validacion de Datos del usuario al crearlo
 	def create(self, validated_data):
-		print(validated_data)
 		username 	= validated_data['username']
 		email 		= validated_data['email']
 		password 	= validated_data['password']
