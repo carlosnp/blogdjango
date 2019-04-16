6ef88af9ee21f24ca0c2ade0e9e6c3f5bbd60ea5
machinery <machinery.e@gmail.com>
2019-04-16 13:46:21 -0400

    registra correctamente la password del usuario

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -16,9 +16,21 @@ class UserCreateSerializer(ModelSerializer):
 		model = User
 		fields = [
 			'username',
+			'email',
 			'password',
-			'email'
 		]
-		extra_kwargs = {"password":
-							{"write_only": True}
+		extra_kwargs = {"password": {"write_only": True}
 		}
+
+	# Validacion de Datos del usuario al crearlo
+	def create(self, validated_data):
+		print(validated_data)
+		username 	= validated_data['username']
+		email 		= validated_data['email']
+		password 	= validated_data['password']
+		# Objeto usuario
+		user_obj 	= User(username=username, email=email)
+		# Para guardar correctamente la contraseña
+		user_obj.set_password(password)
+		user_obj.save()
+		return validated_data
